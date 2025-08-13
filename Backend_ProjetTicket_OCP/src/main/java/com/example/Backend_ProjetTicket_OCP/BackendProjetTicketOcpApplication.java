4ef50694f9dab2e0e25d47c751e73c3f5c43ce19
lolobna <loubnamounir02@gmail.com>
2025-08-13 14:56:33 +0100

    ajout de la version initiale du backend

---
+++ Backend_ProjetTicket_OCP/src/main/java/com/example/Backend_ProjetTicket_OCP/BackendProjetTicketOcpApplication.java
@@ -0,0 +1,13 @@
+package com.example.Backend_ProjetTicket_OCP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProjetTicketOcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProjetTicketOcpApplication.class, args);
+	}
+
+}
---
+++ Backend_ProjetTicket_OCP/src/test/java/com/example/Backend_ProjetTicket_OCP/BackendProjetTicketOcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Backend_ProjetTicket_OCP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProjetTicketOcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
