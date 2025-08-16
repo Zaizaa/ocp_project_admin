4d8fb361f5fa174102102d54ff0ce29df0e07756
salma <hadadiasalma11@gmail.com>
2025-08-16 17:20:38 +0100

    Ajout de backend gestion user

--- Backend_ProjetTicket_OCP/src/main/java/com/example/Backend_ProjetTicket_OCP/BackendProjetTicketOcpApplication.java
+++ Backend_ProjetTicket_OCP/src/main/java/com/example/Backend_ProjetTicket_OCP/BackendProjetTicketOcpApplication.java
@@ -1,12 +1,15 @@
 package com.example.Backend_ProjetTicket_OCP;
 
 
-import com.example.Backend_ProjetTicket_OCP.Entite.Installation;
-import com.example.Backend_ProjetTicket_OCP.Repo.InstallationRepository;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.Backend_ProjetTicket_OCP.Entite.Installation;
+import com.example.Backend_ProjetTicket_OCP.Entite.User;
+import com.example.Backend_ProjetTicket_OCP.Repo.InstallationRepository;
+import com.example.Backend_ProjetTicket_OCP.Repo.UserRepository;
 
 @SpringBootApplication
 public class BackendProjetTicketOcpApplication {
@@ -20,12 +23,26 @@ public class BackendProjetTicketOcpApplication {
 		return args -> {
 			if (installationRepository.count() == 0) {
 				installationRepository.save(new Installation("Station de pompage", "Zone Nord"));
-				installationRepository.save(new Installation("Centrale électrique", "Zone Sud"));
-				installationRepository.save(new Installation("Atelier mécanique", "Zone Est"));
-				installationRepository.save(new Installation("Poste de sécurité", "Zone Ouest"));
+				installationRepository.save(new Installation("Centrale �lectrique", "Zone Sud"));
+				installationRepository.save(new Installation("Atelier m�canique", "Zone Est"));
+				installationRepository.save(new Installation("Poste de s�curit�", "Zone Ouest"));
 				installationRepository.save(new Installation("Zone de stockage", "Centre"));
 			}
+			
 		};
 	}
-
+	// Initialisation des utilisateurs
+    @Bean
+    CommandLineRunner initUserDatabase(UserRepository userRepository) {
+        return args -> {
+            if (userRepository.count() == 0) {
+                userRepository.save(new User("HADADIA", "Salma", "AB123456", "Salma.HADADIA@email.com", "password123", "123 Rue de la Paix", "admin"));
+				 userRepository.save(new User("ZAGRAHI", "Nouhaila", "AB123457", "Nouhaila.ZAGRAHI@email.com", "password123", "123 Rue de la Paix", "admin"));
+                userRepository.save(new User("ZAIZAA", "Hanaa", "CD789012", "Hanaa.ZAIZAA@email.com", "password123", "456 Avenue des Champs", "chef équipe"));
+                userRepository.save(new User("MOUNIR", "Loubna", "EF345678", "Loubna.MOUNIR@email.com", "password123", "789 Boulevard Central", "declarant"));
+            }
+        };
+    }
 }
+
+
