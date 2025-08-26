5acf0cce1a1334a79415b6cb87ba7d218445d2da
lolobna <loubnamounir02@gmail.com>
2025-08-26 08:47:08 +0100

    executer tous le projet avec docker

--- free-nextjs-admin-dashboard-main/next.config.ts
+++ free-nextjs-admin-dashboard-main/next.config.ts
@@ -1,9 +1,17 @@
 import type { NextConfig } from "next";
 
-
 const nextConfig: NextConfig = {
-  /* config options here */
-   eslint: {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8080",
+        pathname: "/**",
+      },
+    ],
+  },
+  eslint: {
     ignoreDuringBuilds: true,
   },
   webpack(config) {
@@ -12,7 +20,7 @@ const nextConfig: NextConfig = {
       use: ["@svgr/webpack"],
     });
     return config;
-  },
+  }
 };
 
 export default nextConfig;
